0f6812bd6e2d221d5565ec60c868941f5ff36adc
Mahesh Talluri <mtalluri563@gmail.com>
2025-04-23 12:04:11 +0530

    mongodb config added

--- server.js
+++ server.js
@@ -13,18 +13,14 @@ app.use(cors());
 const transaction = require("./routes/transaction");
 const categoryRoutes = require("./routes/categories");
 const budgetRoutes = require("./routes/budgets");
+const connectDB = require('./db');
 
 
 // Middleware
 app.use(express.json());
 
 // MongoDB Connection
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-  .then(() => console.log("Connected to MongoDB"))
-  .catch((err) => console.error("Failed to connect to MongoDB", err));
+connectDB()
 
 // Routes
 app.use("/api/transactions", transaction);
